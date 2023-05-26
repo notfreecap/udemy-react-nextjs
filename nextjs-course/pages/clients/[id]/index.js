1c0e4f67d311c22518f8148db539bc5d5e6b2a2e
notfreecap <andresrenew@gmail.com>
2023-05-26 15:16:14 -0500

    navigation

--- nextjs-course/pages/clients/[id]/index.js
+++ nextjs-course/pages/clients/[id]/index.js
@@ -1,3 +1,20 @@
+import { useRouter } from "next/router";
+
 export default function ClientProjectPage() {
-  return <h1>The clients of a given project</h1>;
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientproject]",
+      query: { id: "demo", clientproject: "heyyy" },
+    });
+  }
+
+  return (
+    <div>
+      <h1>The clients of a given project</h1>
+      <button onClick={loadProjectHandler}>Load project A</button>
+      <button onClick={loadProjectHandler}>Load project B</button>
+    </div>
+  );
 }
